75b1af18ca9850e0bfe74effc42da8fea0e799e0
sgerliano <110694611+sgerliano@users.noreply.github.com>
2023-08-13 18:01:31 -0300

    Questões Atualizadas

---
+++ prova1 lab/Q3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Declaração da função para encontrar trincas de lados pitagóricos
+void ladosPitagoricos(int limit) {
+    // Loop para variar o valor de um dos lados (i)
+    for (int i = 1; i < limit; i++) {
+        // Loop aninhado para variar o valor do outro lado (j)
+        for (int j = 1; j < limit; j++) {
+            // Cálculo dos quadrados dos lados i e j
+            int a = i * i;
+            int b = j * j;
+            
+            // Cálculo da hipotenusa c usando a fórmula de Pitágoras
+            int c = sqrt(a + b);
+            
+            // Verificação se os lados formam um triângulo retângulo
+            if (a + b == c * c) {
+                // Impressão dos valores dos lados formando a trinca pitagórica
+                printf("%d %d %d\n", i, j, c);
+            }
+        }
+    }
+}
+
+int main() {
+    // Definição do limite máximo para busca de trincas
+    int limit = 20;
+    
+    // Chamada da função para encontrar e imprimir trincas de lados pitagóricos
+    ladosPitagoricos(limit);
+    
+    // Indicação de saída bem-sucedida
+    return 0;
+}
--- prova1 lab/Q7.c
+++ prova1 lab/Q7.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int function(int m, int n){
-    if(n == 1){
+// Função recursiva para calcular f(m, n)
+int calculate(int m, int n) {
+    // Caso base: se n for igual a 1, retorna m + 1
+    if (n == 1) {
         return m + 1;
-    } else if(m == 1){
+    }
+    // Caso base alternativo: se m for igual a 1, retorna n + 1
+    else if (m == 1) {
         return n + 1;
-    } else{
-        return function(m, n-1) + function(m-1, n);
+    }
+    // Caso geral: utiliza recursão para calcular f(m, n)
+    else {
+        // Chamada recursiva para f(m, n - 1)
+        int result1 = calculate(m, n - 1);
+        // Chamada recursiva para f(m - 1, n)
+        int result2 = calculate(m - 1, n);
+        // Retorna a soma dos resultados das chamadas recursivas
+        return result1 + result2;
     }
 }
 
-int main(){
+int main() {
     int m, n;
-
-    printf("Valor de M: ");
+    
+    // Solicita e lê os valores de m e n
+    printf("Informe o valor de m: ");
     scanf("%d", &m);
-
-    printf("Valor de N: ");
+    
+    printf("Informe o valor de n: ");
     scanf("%d", &n);
-    int func;
-    func = function(m, n);
-    // printf(func);
+
+    // Calcula f(m, n)
+    int result = calculate(m, n);
+    
+    // Imprime o resultado
+    printf("O resultado de f(%d, %d) é %d\n", m, n, result);
+
+    return 0;
 }
