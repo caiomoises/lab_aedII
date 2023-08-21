f667637ac789352f3a6730ea1879e795ee320495
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-08-21 10:01:23 -0300

    update

--- prova1 lab/Q3.c
+++ prova1 lab/Q3.c
@@ -5,9 +5,9 @@
 // Declaração da função para encontrar trincas de lados pitagóricos
 void ladosPitagoricos(int limit) {
     // Loop para variar o valor de um dos lados (i)
-    for (int i = 1; i < limit; i++) {
+    for (int i = 1; i <= limit; i++) {
         // Loop aninhado para variar o valor do outro lado (j)
-        for (int j = 1; j < limit; j++) {
+        for (int j = 1; j <= limit; j++) {
             // Cálculo dos quadrados dos lados i e j
             int a = i * i;
             int b = j * j;
@@ -26,7 +26,7 @@ void ladosPitagoricos(int limit) {
 
 int main() {
     // Definição do limite máximo para busca de trincas
-    int limit = 20;
+    int limit = 25;
     
     // Chamada da função para encontrar e imprimir trincas de lados pitagóricos
     ladosPitagoricos(limit);
